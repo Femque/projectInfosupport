a500107a9b3153475e1fc6a2ce6081d5f82ad62a
FemkeHofland <femke.hofland@hva.nl>
2020-10-15 12:06:03 +0200

    start login and footer

--- infosupport/src/app/app.module.ts
+++ infosupport/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CalenderComponent } from './components/calender/calender.component';
+import {FooterComponent} from "./components/footer/footer.component";
+import {LoginComponent} from "./components/login/login.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
-    CalenderComponent
+    NavbarComponent,
+    CalenderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
