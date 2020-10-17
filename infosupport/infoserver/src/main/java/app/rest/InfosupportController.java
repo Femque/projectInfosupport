4d529b297bd15d34a0494890f480c74ad3b54e6a
jesse <jesse.bijma@hva.nl>
2020-10-17 14:29:53 +0200

    fixed cors

--- infosupport/infoserver/src/main/java/app/rest/InfosupportController.java
+++ infosupport/infoserver/src/main/java/app/rest/InfosupportController.java
@@ -4,10 +4,7 @@ package app.rest;
 import app.models.Appointment;
 import app.repositories.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +15,11 @@ public class InfosupportController {
   @Autowired
   public InfosupportController(AppointmentRepository repo) {this.repo = repo;}
 
+  @CrossOrigin
   @GetMapping("/appointments")
   public List<Appointment> getAllAppointments() { return repo.findAll(); }
 
+  @CrossOrigin
   @PostMapping("/appointments/create")
   public Appointment createAppointment(@RequestBody Appointment appointment) { return repo.createAppointment(appointment); }
 }
