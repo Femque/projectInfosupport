1b1a7a7f83f0134be01306e77d6098dc98cc9d2a
FemkeHofland <femke.hofland@hva.nl>
2020-10-26 11:34:40 +0100

    Users models

--- infosupport/infoserver/src/main/java/app/models/GP.java
+++ infosupport/infoserver/src/main/java/app/models/GP.java
@@ -1,13 +1,50 @@
 package app.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "General_practitioner")
 public class GP extends User{
 
-  private String bigCode;
+  @Id
+  @Column(name = "big-code")
+  private String big_code;
+
+  @Column(name = "specialty")
   private String specialty;
 
-  public GP(long id, String firstname, String lastname, String email, int phonenumber, String password, String bigCode, String specialty) {
-    super(id, firstname, lastname, email, phonenumber, password);
-    this.bigCode = bigCode;
+  @Column(name = "user_id")
+  private Integer user_id;
+
+  public GP() {
+
+  }
+
+  public String getBig_code() {
+    return big_code;
+  }
+
+  public void setBig_code(String big_code) {
+    this.big_code = big_code;
+  }
+
+  public String getSpecialty() {
+    return specialty;
+  }
+
+  public void setSpecialty(String specialty) {
     this.specialty = specialty;
+  }
+
+  @Override
+  public Integer getUser_id() {
+    return user_id;
+  }
+
+  public void setUser_id(Integer user_id) {
+    this.user_id = user_id;
   }
 }
--- infosupport/infoserver/src/main/java/app/models/Patient.java
+++ infosupport/infoserver/src/main/java/app/models/Patient.java
@@ -1,15 +1,61 @@
 package app.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity(name = "Patient")
 public class Patient extends User {
 
+  @Id
+  @Column(name = "user_id")
+  private Integer user_id;
+
+  @Column(name = "dateOfBirth")
   private LocalDate dateOfBirth;
+
+  @Column(name = "gender")
   private String gender;
 
-  public Patient(long id, String firstname, String lastname, String email, int phonenumber, String password, LocalDate dateOfBirth, String gender) {
-    super(id, firstname, lastname, email, phonenumber, password);
+  @Column(name = "allergies")
+  private String allergies;
+
+  public Patient(){
+
+  }
+
+  @Override
+  public Integer getUser_id() {
+    return user_id;
+  }
+
+  @Override
+  public void setUser_id(Integer user_id) {
+    this.user_id = user_id;
+  }
+
+  public LocalDate getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public void setDateOfBirth(LocalDate dateOfBirth) {
     this.dateOfBirth = dateOfBirth;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
     this.gender = gender;
+  }
+
+  public String getAllergies() {
+    return allergies;
+  }
+
+  public void setAllergies(String allergies) {
+    this.allergies = allergies;
   }
 }
