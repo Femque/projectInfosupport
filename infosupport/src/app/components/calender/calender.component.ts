6fa2523d028486af8923bb434420433f79f1cf9e
bijmajm <jesse.bijma@hva.nl>
2020-10-29 19:55:57 +0100

    change :)

--- infosupport/src/app/components/calender/calender.component.ts
+++ infosupport/src/app/components/calender/calender.component.ts
@@ -4,6 +4,9 @@ import {CalendarService} from "./calendar.service";
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {Observable} from "rxjs";
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 @Component({
   selector: 'app-calender',
@@ -32,8 +35,68 @@ export class CalenderComponent implements OnInit {
     document.addEventListener('DOMContentLoaded', function() {
       const calendarEl = document.getElementById('calendar');
 
-      const calendar = new Calendar(calendarEl, {
-        plugins: [dayGridPlugin]
+      let calendar = new Calendar(calendarEl, {
+        plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+        },
+        initialDate: '2018-01-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        dayMaxEvents: true, // allow "more" link when too many events
+        events: [
+          {
+            title: 'All Day Event',
+            start: '2018-01-01',
+          },
+          {
+            groupId: '999',
+            title: 'Repeating Event',
+            start: '2018-01-09T16:00:00'
+          },
+          {
+            groupId: '999',
+            title: 'Repeating Event',
+            start: '2018-01-16T16:00:00'
+          },
+          {
+            title: 'Conference',
+            start: '2018-01-11',
+            end: '2018-01-13'
+          },
+          {
+            title: 'Meeting',
+            start: '2018-01-12T10:30:00',
+            end: '2018-01-12T12:30:00'
+          },
+          {
+            title: 'Lunch',
+            start: '2018-01-12T12:00:00'
+          },
+          {
+            title: 'Meeting',
+            start: '2018-01-12T14:30:00'
+          },
+          {
+            title: 'Happy Hour',
+            start: '2018-01-12T17:30:00'
+          },
+          {
+            title: 'Dinner',
+            start: '2018-01-12T20:00:00'
+          },
+          {
+            title: 'Birthday Party',
+            start: '2018-01-13T07:00:00'
+          },
+          {
+            title: 'Click for Google',
+            url: 'http://google.com/',
+            start: '2018-01-28'
+          }
+        ]
       });
 
       calendar.render();
