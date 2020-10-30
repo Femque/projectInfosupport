ad386f052a42f9c0f0040a7e0666773a3178bfd6
jesse <jesse.bijma@hva.nl>
2020-10-30 14:24:58 +0100

    change calendar

--- infosupport/src/app/components/calender/calender.component.ts
+++ infosupport/src/app/components/calender/calender.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {Appointment} from "../../models/appointment";
 import {CalendarService} from "./calendar.service";
-import { Calendar } from '@fullcalendar/core';
+import {Calendar} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import {Observable} from "rxjs";
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import { finalize } from "rxjs/operators";
+import {log} from "util";
 
 @Component({
   selector: 'app-calender',
@@ -16,27 +18,30 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 })
 
 export class CalenderComponent implements OnInit {
-
   appointments: Appointment[];
-
+  length: number;
 
   constructor(private calendarService: CalendarService) {
   }
 
-  ngOnInit() {
-      this.createCalendar();
+  async ngOnInit() {
+    this.appointments = [];
+    await this.getAppointments();
+    // this.createCalendar(this.appointments);
   }
 
   createAppointment(appointment: Appointment): Observable<Appointment> {
     return this.calendarService.createAppointment(appointment)
   }
 
-  createCalendar() {
-    document.addEventListener('DOMContentLoaded', function() {
+  createCalendar(appointments: any[]) {
+    console.log('creating calendar');
+
+    document.addEventListener('DOMContentLoaded', function () {
       const calendarEl = document.getElementById('calendar');
 
       let calendar = new Calendar(calendarEl, {
-        plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+        plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
@@ -46,78 +51,59 @@ export class CalenderComponent implements OnInit {
         navLinks: true, // can click day/week names to navigate views
         editable: true,
         dayMaxEvents: true, // allow "more" link when too many events
-        events: [
-          {
-            title: 'All Day Event',
-            start: '2018-01-01',
-          },
-          {
-            groupId: '999',
-            title: 'Repeating Event',
-            start: '2018-01-09T16:00:00'
-          },
-          {
-            groupId: '999',
-            title: 'Repeating Event',
-            start: '2018-01-16T16:00:00'
-          },
-          {
-            title: 'Conference',
-            start: '2018-01-11',
-            end: '2018-01-13'
-          },
-          {
-            title: 'Meeting',
-            start: '2018-01-12T10:30:00',
-            end: '2018-01-12T12:30:00'
-          },
-          {
-            title: 'Lunch',
-            start: '2018-01-12T12:00:00'
-          },
-          {
-            title: 'Meeting',
-            start: '2018-01-12T14:30:00'
-          },
-          {
-            title: 'Happy Hour',
-            start: '2018-01-12T17:30:00'
-          },
-          {
-            title: 'Dinner',
-            start: '2018-01-12T20:00:00'
-          },
-          {
-            title: 'Birthday Party',
-            start: '2018-01-13T07:00:00'
-          },
-          {
-            title: 'Click for Google',
-            url: 'http://google.com/',
-            start: '2018-01-28'
-          }
-        ]
       });
+
+
+      // 'title': data[i].patient_user_id,
+      //   'description': data[i].description,
+      //   'start': data[i].start_time,
+      //   'allDay': false
+
+      console.log("for");
+      console.log(appointments);
+      console.log(appointments.length);
+      for (let i = 0; i < appointments.length; i++) {
+        console.log(i);
+        // calendar.addEvent(appointments[i].toJson());
+      }
 
       calendar.render();
     });
   }
 
-  // getAppointments(): any {
-  //   this.calendarService.getAppointments()
-  //     .subscribe(data => {
-  //       for (let i = 0; i < data.length; i++) {
-  //         let newAppointment = new Appointment(
-  //           data[i].id,
-  //           data[i].patient,
-  //           data[i].gp,
-  //           data[i].startTime,
-  //           data[i].endTime,
-  //           data[i].description,
-  //           data[i].location
-  //         )
-  //         this.appointments.push(newAppointment);
-  //       }});
-  // }
+  async getAppointments(): Promise<any> {
+    this.calendarService.getAppointments()
+      .subscribe(data => {
+        for (let i = 0; i < data.length; i++) {
+          // let startDateTime = new Date(data[i].start_time).toLocaleString();
+          // let timeDateArray = startDateTime.split(" ");
+          //
+          // let startDateString = timeDateArray[0];
+          // let datePieces = startDateString.split("-");
+          // let startDate = datePieces[2] + "-" + datePieces[1] + "-" + datePieces[0];
+          // let startTime = timeDateArray[1];
+
+          // console.log(startDate);
+          // console.log(startTime);
+
+          let newAppointment = new Appointment(
+            data[i].appointment_code,
+            data[i].start_time,
+            data[i].end_time,
+            data[i].is_digital,
+            data[i].description,
+            data[i].location,
+            data[i].is_follow_up,
+            data[i].big_code,
+            data[i].patient_user_id
+          )
+
+          this.appointments.push(newAppointment);
+
+        }
+      }, error => console.log(error));
+
+    console.log("done getting appointments");
+  }
 }
 
