38bd9d456ab679a43a89246bf74a9ca5daa7c389
Femke Hofland <femke.hofland@hva.nl>
2020-10-30 16:36:34 +0100

    Login and session

--- infosupport/src/app/components/login/login.service.ts
+++ infosupport/src/app/components/login/login.service.ts
@@ -3,18 +3,27 @@ import {HttpClient} from "@angular/common/http";
 import {BehaviorSubject, config, Observable} from "rxjs";
 import {Router} from "@angular/router";
 
-
-
 @Injectable({ providedIn: "root"})
 export class LoginService {
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { }
+  constructor() { }
 
-  login(data): Observable<any> {
-    return this.http.post(`$http://localhost:8080/users/login`, data)
+  authenticate(username, password) {
+    if (username === "" && password === "") {
+      sessionStorage.setItem('username', username)
+      return true;
+    } else {
+      return false;
+    }
   }
 
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('username')
+    console.log(!(user === null))
+    return !(user === null)
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username')
+  }
 }
