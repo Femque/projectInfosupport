e64171f4293a62fc5eb6ab8326d771daa114b546
Femke Hofland <femke.hofland@hva.nl>
2020-11-02 15:13:09 +0100

    .

--- infosupport/src/app/components/login/login.service.ts
+++ infosupport/src/app/components/login/login.service.ts
@@ -2,16 +2,27 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {BehaviorSubject, config, Observable} from "rxjs";
 import {Router} from "@angular/router";
+import {User} from "../../models/user";
 
 @Injectable({ providedIn: "root"})
 export class LoginService {
+  public user: Observable<User>;
+  private userSubject: BehaviorSubject<User>;
 
-  constructor() { }
+  usersUrl = 'http://localhost:8080/user';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
 
   authenticate(username, password) {
     if (username === "" && password === "") {
       sessionStorage.setItem('username', username)
-      return true;
+      return this.http.post<User>(this.usersUrl + "/login", username);
     } else {
       return false;
     }
@@ -24,6 +35,7 @@ export class LoginService {
   }
 
   logOut() {
-    sessionStorage.removeItem('username')
+    sessionStorage.removeItem('username');
+    this.userSubject.next(null);
   }
 }
