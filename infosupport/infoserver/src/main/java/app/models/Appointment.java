35de1fa1e2b3f1067108389f78f1434a2ec54c7d
Bram Osborne <Bram.osborne@hva.nl>
2020-11-03 14:59:05 +0100

    appointment backend fix

--- infosupport/infoserver/src/main/java/app/models/Appointment.java
+++ infosupport/infoserver/src/main/java/app/models/Appointment.java
@@ -8,71 +8,17 @@ import java.time.LocalDateTime;
 public class Appointment {
 
   @Id
-  @Column(name = "appointment_code")
   private Integer appointment_code;
-
-  @Column(name = "start_time")
   private LocalDateTime start_time;
-
-  @Column(name = "end_time")
   private LocalDateTime end_time;
-
-  @Column(name = "is_digital")
   private Boolean is_digital;
-
-  @Column(name = "description")
   private String description;
-
-  @Column(name = "location")
   private String location;
-
-  @Column(name = "is_follow_up")
   private Boolean is_follow_up;
-
-  @Column(name = "big_code")
   private Integer big_code;
-
-  @Column(name = "patient_user_id")
   private Integer patient_user_id;
 
   public Appointment() {
-  }
-
-  public void setAppointment_code(Integer appointment_code) {
-    this.appointment_code = appointment_code;
-  }
-
-
-  public void setStart_time(LocalDateTime start_time) {
-    this.start_time = start_time;
-  }
-
-  public void setEnd_time(LocalDateTime end_time) {
-    this.end_time = end_time;
-  }
-
-  public void setIs_digital(boolean is_digital) {
-    this.is_digital = is_digital;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public void setLocation(String location) {
-    this.location = location;
-  }
-
-  public void setIs_follow_up(Boolean is_follow_up) {
-    this.is_follow_up = is_follow_up;
-  }
-
-  public void setBig_code(Integer big_code) {
-    this.big_code = big_code;
-  }
-
-  public void setPatient_user_id(Integer patient_user_id) {
-    this.patient_user_id = patient_user_id;
   }
 
   public LocalDateTime getStart_time() {
