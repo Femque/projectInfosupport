d7227642a9ebd7f835fb26f6bc166cd51d946680
Femke Hofland <femke.hofland@hva.nl>
2020-11-04 13:42:13 +0100

    Spring boot session management

--- infosupport/infoserver/src/main/java/app/InfoserverApplication.java
+++ infosupport/infoserver/src/main/java/app/InfoserverApplication.java
@@ -1,5 +1,7 @@
 package app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,8 +9,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @SpringBootApplication
 public class InfoserverApplication {
 
+  private final static Logger log = LoggerFactory.getLogger(InfoserverApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(InfoserverApplication.class, args);
+    log.info("Spring-boot-session-management application started succesfully");
   }
 
 }
