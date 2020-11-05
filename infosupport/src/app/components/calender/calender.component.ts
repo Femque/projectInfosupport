c05c666197cbae57fb0630b92b2a6d263c171d71
Bram Osborne <Bram.osborne@hva.nl>
2020-11-05 15:59:25 +0100

    merge front end fix

--- infosupport/src/app/components/calender/calender.component.ts
+++ infosupport/src/app/components/calender/calender.component.ts
@@ -25,19 +25,6 @@ export class CalenderComponent implements OnInit {
   closeResult = '';
   appointmentForm;
 
-  @ViewChild('create')
-  create;
-
-  constructor(private calendarService: CalendarService, private modalService: NgbModal, private formBuilder: FormBuilder) {
-    this.appointmentForm = this.formBuilder.group({
-      title: [''],
-      description: [''],
-      location: [''],
-      start: [''],
-      end: [''],
-      is_digital: [false],
-      is_followup: [false]
-    })
   selectedAppointment: any;
 
   patient_user_id: number;
@@ -50,170 +37,201 @@ export class CalenderComponent implements OnInit {
   big_code: number;
   appointment_code: number;
 
+  @ViewChild('create')
+  create;
   @ViewChild('content')
   content;
 
-  constructor(private calendarService: CalendarService, private modalService: NgbModal) {
-  }
+  constructor(private calendarService: CalendarService, private modalService: NgbModal, private formBuilder: FormBuilder) {
+    this.appointmentForm = this.formBuilder.group({
+      title: [''],
+      description: [''],
+      location: [''],
+      start: [''],
+      end: [''],
+      is_digital: [false],
+      is_followup: [false]
+    })}
 
-  async ngOnInit() {
-    await this.getAppointments();
-  }
+    async ngOnInit() {
+      await this.getAppointments();}
 
-  open() {
-    this.modalService.open(this.create, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      console.log(result);
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  openUpdate(event: any) {
-    this.selectedAppointment = event;
-    this.patient_user_id = event.patient_user_id
-    this.location = event.location
-    this.start_time = event.start_time
-    this.end_time = event.end_time
-    this.is_follow_up = event.is_follow_up
-    this.is_digital = event.is_digital
-    this.description = event.description
-    this.big_code = event.big_code
-    this.appointment_code = event.appointment_code
-
-    console.log(event)
-    this.modalService.open(this.content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
-  async createCalendar(appointments: Appointment[]) {
-    console.log('creating calendar');
-
-    const calendarEl = document.getElementById('calendar');
-
-    let calendar = new Calendar(calendarEl,
-
-      {
-        plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
-        headerToolbar: {
-          left: 'prev,next today add_event',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-        },
-        customButtons: {
-          add_event: {
-            text: 'Nieuwe afspraak',
-            click: () => this.open()
-          }
-        },
-        eventClick: (event,) =>this.openUpdate(event.event._def.extendedProps),
-        height: 700,
-        navLinks: true, // can click day/week names to navigate views
-        editable: true,
-        dayMaxEvents: true, // allow "more" link when too many events,
-        eventDisplay: "block"
+    open()
+    {
+      this.modalService.open(this.create, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        console.log(result);
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
+    }
 
-    for (let i = 0; i < appointments.length; i++) {
-      calendar.addEvent({
-        title: appointments[i].patient_user_id.toLocaleString(),
-        start: appointments[i].start_time,
-        end: appointments[i].end_time,
-        start_time: appointments[i].start_time,
-        end_time: appointments[i].end_time,
-        is_digital: appointments[i].is_digital,
-        description: appointments[i].description,
-        location: appointments[i].location,
-        is_follow_up: appointments[i].is_follow_up,
-        big_code: appointments[i].big_code,
-        appointment_code: appointments[i].appointment_code,
-        patient_user_id: appointments[i].patient_user_id
+    openUpdate(event: any)
+    {
+      this.selectedAppointment = event;
+      this.patient_user_id = event.patient_user_id
+      this.location = event.location
+      this.start_time = event.start_time
+      this.end_time = event.end_time
+      this.is_follow_up = event.is_follow_up
+      this.is_digital = event.is_digital
+      this.description = event.description
+      this.big_code = event.big_code
+      this.appointment_code = event.appointment_code
+
+      console.log(event)
+      this.modalService.open(this.content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
+    }
+
+  private getDismissReason(reason : any): string
+    {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return `with: ${reason}`;
+      }
+    }
+
+    async createCalendar(appointments: Appointment[])
+    {
+      console.log('creating calendar');
+
+      const calendarEl = document.getElementById('calendar');
+
+      let calendar = new Calendar(calendarEl,
+
+        {
+          plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+          headerToolbar: {
+            left: 'prev,next today add_event',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+          },
+          customButtons: {
+            add_event: {
+              text: 'Nieuwe afspraak',
+              click: () => this.open()
+            }
+          },
+          eventClick: (event,) => this.openUpdate(event.event._def.extendedProps),
+          height: 700,
+          navLinks: true, // can click day/week names to navigate views
+          editable: true,
+          dayMaxEvents: true, // allow "more" link when too many events,
+          eventDisplay: "block"
+        });
+
+      for (let i = 0; i < appointments.length; i++) {
+        calendar.addEvent({
+          title: appointments[i].patient_user_id.toLocaleString(),
+          start: appointments[i].start_time,
+          end: appointments[i].end_time,
+          start_time: appointments[i].start_time,
+          end_time: appointments[i].end_time,
+          is_digital: appointments[i].is_digital,
+          description: appointments[i].description,
+          location: appointments[i].location,
+          is_follow_up: appointments[i].is_follow_up,
+          big_code: appointments[i].big_code,
+          appointment_code: appointments[i].appointment_code,
+          patient_user_id: appointments[i].patient_user_id
+        });
+
+      }
+      calendar.render();
 
     }
-    calendar.render();
 
-  }
-
-  createAppointment(appointmentdata) {
-    let appointment = new Appointment(appointmentdata.start, appointmentdata.end, appointmentdata.is_digital,
-      appointmentdata.description, appointmentdata.location, appointmentdata.is_followup, 321, 123);
-    console.log(appointment);
+    createAppointment(appointmentdata)
+    {
+      let appointment = new Appointment(appointmentdata.start, appointmentdata.end, appointmentdata.is_digital,
+        appointmentdata.description, appointmentdata.location, appointmentdata.is_followup, 321, 123);
+      console.log(appointment);
       this.calendarService.createAppointment(appointment)
-      .subscribe( data => {
-       let newAppointment = new Calendar_appointment(data.patient_user_id.toString(), data.start_time, data.end_time);
-        this.appointments.push(newAppointment);
-      })
-  }
+        .subscribe(data => {
+          let newAppointment = new Appointment(
+            data.start_time,
+            data.end_time,
+            data.is_digital,
+            data.description,
+            data.location,
+            data.is_follow_up,
+            data.big_code,
+            data.patient_user_id
+          );
+          this.appointments.push(newAppointment);
+        })
+    }
 
-  async getAppointments() {
-    this.calendarService.getAppointments()
-      .subscribe(data => {
-        for (let i = 0; i < data.length; i++) {
-          let appointmentsTest = new Appointment(data[i].appointment_code, data[i].start_time, data[i].end_time, data[i].is_digital,
-            data[i].description, data[i].location, data[i].is_follow_up, data[i].big_code, data[i].patient_user_id)
+    async
+    getAppointments()
+    {
+      this.calendarService.getAppointments()
+        .subscribe(data => {
+          for (let i = 0; i < data.length; i++) {
+            let appointmentsTest = new Appointment(data[i].start_time, data[i].end_time, data[i].is_digital,
+              data[i].description, data[i].location, data[i].is_follow_up, data[i].big_code, data[i].patient_user_id)
 
-          this.appointments.push(appointmentsTest);
+            this.appointments.push(appointmentsTest);
+          }
+        }, error => console.log(error), () => this.createCalendar(this.appointments));
+    }
+
+    updateAppoitment()
+    {
+      let updatedAppointment = new Appointment(
+        this.start_time,
+        this.end_time,
+        this.is_digital,
+        this.description,
+        this.location,
+        this.is_follow_up,
+        this.big_code,
+        this.patient_user_id
+      )
+      console.log(updatedAppointment)
+      console.log(this.location)
+      this.calendarService.updateAppointment(updatedAppointment).subscribe(
+        (data) => {
+        }, (error) => {
+          alert('HTTP Error: Status' + error.status + '-' + error.message)
         }
-      }, error => console.log(error), () => this.createCalendar(this.appointments));
-  }
+      )
+      this.modalService.dismissAll()
+      window.location.reload()
 
-  updateAppoitment(){
-    let updatedAppointment = new Appointment(
-      this.appointment_code,
-      this.start_time,
-      this.end_time,
-      this.is_digital,
-      this.description,
-      this.location,
-      this.is_follow_up,
-      this.big_code,
-      this.patient_user_id
-    )
-    console.log(updatedAppointment)
-    console.log(this.location)
-    this.calendarService.updateAppointment(updatedAppointment).subscribe(
-      (data) => {}, (error) => {
-        alert('HTTP Error: Status' + error.status+ '-' + error.message)
-      }
-    )
+    }
+
+    deleteAppointment()
+    {
+      let deleted = new Appointment(
+        this.start_time,
+        this.end_time,
+        this.is_digital,
+        this.description,
+        this.location,
+        this.is_follow_up,
+        this.big_code,
+        this.patient_user_id
+      )
+
+      this.calendarService.deleteAppointment(deleted).subscribe(
+        (data) => {
+        }, (error) => {
+          alert('HTTP Error: Status' + error.status + '-' + error.message)
+        }
+      )
+      this.modalService.dismissAll()
+      window.location.reload()
+    }
+
+    cancelUpdate(){
     this.modalService.dismissAll()
-    window.location.reload()
-
+    }
   }
-
-  deleteAppointment(){
-    let deleted = new Appointment(
-      this.appointment_code,
-      this.start_time,
-      this.end_time,
-      this.is_digital,
-      this.description,
-      this.location,
-      this.is_follow_up,
-      this.big_code,
-      this.patient_user_id
-    )
-
-    this.calendarService.deleteAppointment(deleted).subscribe(
-      (data) => {}, (error) => {
-        alert('HTTP Error: Status' + error.status+ '-' + error.message)
-      }
-    )
-    this.modalService.dismissAll()
-    window.location.reload()
-  }
-}
