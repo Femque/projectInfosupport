0bbac6760624454ca471f8e1688608faa86f5f8a
Femke Hofland <femke.hofland@hva.nl>
2021-01-11 14:51:16 +0100

    fe testing

--- infosupport-FE/src/app/components/login/login.component.spec.ts
+++ infosupport-FE/src/app/components/login/login.component.spec.ts
@@ -1,11 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {Component, OnInit} from '@angular/core';
-import {LoginService} from "./login.service";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Router} from "@angular/router";
-import {User} from "../../models/user";
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from "@angular/router/testing";
+import {LoginComponent} from './login.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
-import { LoginComponent } from './login.component';
+/**
+ * @author Femke Hofland
+ * fe Login Component tests
+ */
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -14,7 +17,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [HttpClientModule, RouterTestingModule, HttpClientTestingModule, FormsModule, ReactiveFormsModule],
+      providers: [FormBuilder]
     })
       .compileComponents();
   });
@@ -28,6 +33,58 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  function updateForm(email, password) {
+    component.loginForm.controls['email'].setValue(email);
+    component.loginForm.controls['password'].setValue(password);
+  }
+
+  it('Test_1: Should find login form and submit button', () => {
+
+    // Arrange
+    const emailContainer: HTMLInputElement = componentHtml.querySelector('#email');
+    const passwordContainer: HTMLInputElement = componentHtml.querySelector('#password');
+    const submitBtn: HTMLButtonElement = componentHtml.querySelector('#loginButton');
+
+    // Act
+    updateForm('femkehofland@hotmail.nl', 'wachtwoord2');
+    fixture.detectChanges();
+
+    // Assert
+    expect(emailContainer).toBeDefined();
+    expect(passwordContainer).toBeDefined();
+    expect(submitBtn).toBeDefined();
+  });
+
+  it('Test_2: Should expect user to have user_id and user_role after login', () => {
+    // Arrange
+    const submitBtn: HTMLButtonElement = componentHtml.querySelector('#loginButton');
+
+    // Act
+    updateForm('femkehofland@hotmail.nl', 'wachtwoord2');
+    fixture.detectChanges();
+    submitBtn.click();
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.getUser_id).toBeDefined();
+    expect(component.getUserRole).toBeDefined();
+  });
+
+  it('Test_3: Should expect error when email and password are wrong', () => {
+    // Arrange
+    const submitBtn: HTMLButtonElement = componentHtml.querySelector('#loginButton');
+    const errorMsg = fixture.debugElement.nativeElement.querySelector('.alert-error');
+
+    // Act
+    updateForm('wrong@wrong.nl', 'wrong');
+    fixture.detectChanges();
+    submitBtn.click();
+    fixture.detectChanges();
+
+    // Assert
+    expect(errorMsg).toBeDefined();
   });
 });
 
--- infosupport-FE/src/app/components/login/login.service.spec.ts
+++ infosupport-FE/src/app/components/login/login.service.spec.ts
@@ -1,7 +1,15 @@
 import {TestBed} from '@angular/core/testing';
 import {LoginService} from './login.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {HttpClient} from "@angular/common/http";
+import {of} from "rxjs";
 import {User} from "../../models/user";
+import {response} from "express";
+
+/**
+ * @author Femke Hofland
+ * fe LoginService tests
+ */
 
 describe('LoginService', () => {
   let service: LoginService;
@@ -20,26 +28,46 @@ describe('LoginService', () => {
 
   beforeEach(() => {
     httpMock.verify();
-    sessionStorage.setItem('user_id', String(2))
   });
 
-  // Service Test_01: get user_data
-  it('Service Test_01: should get user_data', () => {
+  it('Service Test_1: Should perform a post to /login with email and password', () => {
 
-    const userData = [{
-      user_id: 2,
-      firstname: 'Femke',
-      lastname: 'Hofland',
-      email: 'femkehofland@hotmail.nl',
-      phonenumber: '0648461416',
-      password: 'wachtwoord2',
-      dateOfBirth: new Date(1995-3-6),
-      gender: "Vrouw"
-    }];
+    // Arrange
+    const httpClient: jasmine.SpyObj<HttpClient> = jasmine
+      .createSpyObj('http', ['post']);
+    const loginService = new LoginService(httpClient);
+    httpClient.post.and.returnValue(of());
 
-    // service.getUserInfoById().subscribe( (res: User[]) => {
-    //   expect(res.).toEqual('Femke')
-    // })
-  })
+    // Act
+    loginService.loginUserFromRemote({
+      user_id: 2, email: 'femkehofland@hotmail.nl',
+      password: 'wachtwoord2', firstname: 'Femke', lastname: 'Hofland', gender: 'Vrouw',
+      dateOfBirth: new Date(1995 - 3 - 6), phonenumber: '0648461416'
+    });
 
+    // Assert
+    expect(httpClient.post).toHaveBeenCalledWith('http://localhost:8080/user/login', {
+      user_id: 2, email: 'femkehofland@hotmail.nl',
+      password: 'wachtwoord2', firstname: 'Femke', lastname: 'Hofland', gender: 'Vrouw',
+      dateOfBirth: new Date(1995 - 3 - 6), phonenumber: '0648461416'
+    });
+  });
+
+  it('Service Test_2: Should perform a get request and get user_id', () => {
+
+    // Arrange
+    const httpClient: jasmine.SpyObj<HttpClient> = jasmine
+      .createSpyObj('http', ['get']);
+    const loginService = new LoginService(httpClient);
+    httpClient.get.and.returnValue(of());
+
+    // Act
+    loginService.fetchUserId('femkehofland@hotmail.nl').subscribe((res: number) => {
+
+      // Assert
+      expect(res).toEqual(2);
+    });
+
+    expect(httpClient.get).toHaveBeenCalledWith('http://localhost:8080/user/id?email=femkehofland@hotmail.nl');
+  });
 });
